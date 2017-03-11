936bb34a64d7b15850f81e9573d9a95d5e7332ac
silentdeath76 <smeex@mail.ru>
2017-03-11 13:52:14 +0300

    Refactor and bug fix

--- src/vendor/develnext.bundle.yandextranslate.YandexTranslateBundle/bundle/yandextranslate/YandexTranslate.php
+++ src/vendor/develnext.bundle.yandextranslate.YandexTranslateBundle/bundle/yandextranslate/YandexTranslate.php
@@ -1,110 +1,112 @@
 <?php
-    namespace bundle\yandextranslate;
+namespace bundle\yandextranslate;
 
-    use 
-        bundle\http\HttpResponse,
-        bundle\http\HttpClient;
+use
+    bundle\http\HttpResponse,
+    bundle\http\HttpClient;
 
-    // TODO: Был удален класс Y_API как временное решение при невозможности провести финальную компиляцию
+/**
+ * Class YandexTranslate
+ * @package bundle\yandextranslate
+ */
+class YandexTranslate
+{
+    const
+        HOST = "https://translate.yandex.net/api/v1.5/tr.json/",
+        SUCCESS = 200;
 
     /**
-     * Class YandexTranslate
+     * @var string
      */
-    class YandexTranslate  {
-        const
-            HOST = "https://translate.yandex.net/api/v1.5/tr.json/",
-            SUCCESS = 200;
+    protected $apiKey = null;
 
-        /**
-         * @var string
-         */
-        protected $apiKey = null;
-
-        /**
-         * @var HttpClient
-         */
-        protected $http = null;
+    /**
+     * @var HttpClient
+     */
+    protected $http = null;
 
 
-        /**
-         * YandexTranslate constructor.
-         * @param string $apiKey
-         * @throws YandexTranslateException
-         */
-        public function __construct ($apiKey) {
-            $this->setApiKey($apiKey);
-            $this->http = new HttpClient();
-        }
-
-
-        /**
-         * @param string $apiKey
-         * @throws YandexTranslateException
-         */
-        public function setApiKey($apiKey) {
-            $apiKey = trim($apiKey);
-
-            if(isset($apiKey) && !empty($apiKey)) {
-                $this->apiKey = $apiKey;
-            } else {
-                Throw new YandexTranslateException( YandexTranslateException::EMPTY_API_KEY );
-            }
-
-        }
-
-
-        /**
-         * Перевод текста
-         * @param string $translateString
-         * @param string $lang
-         * @return string
-         * @throws YandexTranslateException
-         */
-        public function translate ($translateString, $lang = "en-ru") {
-
-            $url = sprintf('translate?key=%s&lang=%s&format=plain', $this->apiKey, $lang);
-
-            $response = $this->httpPost($url, ["text" => $translateString]);
-
-            if(is_array($response) && $response['code'] == self::SUCCESS) {
-                return $response['text'][0];
-            }
-
-            Throw new YandexTranslateException( $response['code'] );
-        }
-
-        /**
-         * Возвращает список поддерживаемых языков
-         * Return a list supported langs
-         * @param string $ui
-         * @return array
-         * @throws YandexTranslateException
-         */
-        public function getLangs ($ui = "ru") {
-            $url = sprintf('getLangs?key=%s&ui=%s', $this->apiKey, $ui);
-            $response = $this->httpPost($url);
-
-            if(is_array($response) && isset($response['langs'])) {
-                return $response['langs'];
-            }
-
-            Throw new YandexTranslateException( $response['code'] );
-        }
-
-
-
-
-
-        /**
-         * Отправка POST запроса
-         * @param $url
-         * @param $translateString
-         * @return array
-         */
-        final private function httpPost ($url, $postData = []) {
-
-            $httpResponse = $this->http->post(self::HOST . $url, $postData);
-            return json_decode($httpResponse->body(), true);
-
-        }
+    /**
+     * YandexTranslate constructor.
+     * @param string $apiKey
+     * @throws YandexTranslateException
+     */
+    public function __construct($apiKey)
+    {
+        $this->setApiKey($apiKey);
+        $this->http = new HttpClient();
     }
+
+
+    /**
+     * @param string $apiKey
+     * @throws YandexTranslateException
+     */
+    public function setApiKey($apiKey)
+    {
+        $apiKey = trim($apiKey);
+
+        if (isset($apiKey) && !empty($apiKey)) {
+            $this->apiKey = $apiKey;
+        } else {
+            Throw new YandexTranslateException(YandexTranslateException::EMPTY_API_KEY);
+        }
+
+    }
+
+
+    /**
+     * Перевод текста
+     * @param string $translateString
+     * @param string $lang
+     * @return string
+     * @throws YandexTranslateException
+     */
+    public function translate($translateString, $lang = "en-ru")
+    {
+
+        $url = sprintf('translate?key=%s&lang=%s&format=plain', $this->apiKey, $lang);
+
+        $response = $this->httpPost($url, ["text" => $translateString]);
+
+        if (is_array($response) && $response['code'] == self::SUCCESS) {
+            return $response['text'][0];
+        }
+
+        Throw new YandexTranslateException($response['code']);
+    }
+
+    /**
+     * Возвращает список поддерживаемых языков
+     * Return a list supported langs
+     * @param string $ui
+     * @return array
+     * @throws YandexTranslateException
+     */
+    public function getLangs($ui = "ru")
+    {
+        $url = sprintf('getLangs?key=%s&ui=%s', $this->apiKey, $ui);
+        $response = $this->httpPost($url);
+
+        if (is_array($response) && isset($response['langs'])) {
+            return $response['langs'];
+        }
+
+        Throw new YandexTranslateException($response['code']);
+    }
+
+
+    /**
+     * Отправка POST запроса
+     * @param $url
+     * @param array $postData
+     * @return array
+     */
+    final private function httpPost($url, $postData = [])
+    {
+
+        $httpResponse = $this->http->post(self::HOST . $url, $postData);
+        return json_decode($httpResponse->body(), true);
+
+    }
+}
--- src/vendor/develnext.bundle.yandextranslate.YandexTranslateBundle/bundle/yandextranslate/YandexTranslateException.php
+++ src/vendor/develnext.bundle.yandextranslate.YandexTranslateBundle/bundle/yandextranslate/YandexTranslateException.php
@@ -1,36 +1,44 @@
 <?php
-    namespace bundle\yandextranslate;
+namespace bundle\yandextranslate;
+
+/**
+ * Class YandexTranslateException
+ * @package bundle\yandextranslate
+ */
+class YandexTranslateException extends \Exception
+{
+
+    const
+        UNDEFINED_ERROR = 0,
+        EMPTY_API_KEY = 1;
 
     /**
-     * Class YandexTranslateException
+     * @var array
      */
-    class YandexTranslateException extends \Exception {
-
-        const
-            UNDEFINED_ERROR                 = 0,
-            EMPTY_API_KEY                   = 1;
-
-        private $_message = [
-            '401' => "Неправильный API-ключ",
-            '402' => "API-ключ заблокирован",
-            '404' => "Превышено суточное ограничение на объем переведенного текста",
-            '413' => "Превышен максимально допустимый размер текста",
-            '422' => "Текст не может быть переведен",
-            '501' => "Заданное направление перевода не поддерживается",
-            '0'   => "Неизвестная ошибка",
-            '1'   => "API ключ пустой",
-        ];
+    private $_message = [
+        '401' => "Неправильный API-ключ",
+        '402' => "API-ключ заблокирован",
+        '404' => "Превышено суточное ограничение на объем переведенного текста",
+        '413' => "Превышен максимально допустимый размер текста",
+        '422' => "Текст не может быть переведен",
+        '501' => "Заданное направление перевода не поддерживается",
+        '0' => "Неизвестная ошибка",
+        '1' => "API ключ пустой",
+    ];
 
 
+    /**
+     * YandexTranslateException constructor.
+     * @param int|string $errorCode
+     */
+    final public function __construct($errorCode = YandexTranslate::SUCCESS)
+    {
 
-
-        final public function __construct($errorCode = YandexTranslate::SUCCESS) {
-
-            if(array_key_exists($errorCode, $this->_message)) {
-                parent::__construct($this->_message[$errorCode], $errorCode);
-            } else {
-                parent::__construct(self::UNDEFINED_ERROR, $errorCode);
-            }
-
+        if (array_key_exists($errorCode, $this->_message)) {
+            parent::__construct($this->_message[$errorCode], $errorCode);
+        } else {
+            parent::__construct($this->_message[self::UNDEFINED_ERROR], $errorCode);
         }
+
     }
+}
